65ed8041216a38966307178bcbeca627081ad835
adib-hash <adib@ihsan.build>
2026-02-23 19:42:29 -0800

    Create main.jsx

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import Cousin from './Cousin.jsx'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <Cousin />
+  </React.StrictMode>
+)
